c25d11383f9beb8a5e4196f8407456899002342f
anakha <anakha771@gmail.com>
2025-06-21 21:56:06 +0530

    task

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import Login from './components/Login.jsx'
+// import Login from './components/Login.jsx'
 import './App.css'
 import Admnavbar from './components/admin/Admnavbar.jsx'
+import Project from './components/admin/Project.jsx'
+import AdmDash from './components/admin/AdmDash.jsx'
+import Task from './components/admin/Task';
 const App = () => {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Admnavbar/>
+      {/* <AdmDash/> */}
+      {/* <Project/> */}
+      <Task/>
     </div>
   )
 }
